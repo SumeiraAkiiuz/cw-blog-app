2ba4088af0264bfdd272447a3e1fedc3551dcd76
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2021-01-20 14:42:22 -0500

    category and post list serializers added

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from .views import CategoryList, CategoryDetail, PostDetail
 
-# urlpatterns = [
-#     path("", Category, name="category")
-# ]
+urlpatterns = [
+    path("", CategoryList.as_view(), name="category"),
+    path("<category>/", CategoryDetail.as_view(), name="category-detail"),
+    path("post/<title>/", PostDetail.as_view(), name="post"),
+    # path("", CommentList.as_view(), name="comment"),
+    # path("", LikeList.as_view(), name="like"),
+    
+    
+]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Post
+from .serializers import CategorySerializer, CategoryDetailSerializer, PostSerializer
 
-# Create your views here.
+
+class CategoryList(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetail(generics.ListAPIView):
+    serializer_class = CategoryDetailSerializer
+    
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        category = self.kwargs["category"]
+        queryset = queryset.filter(category__name=category)
+        return queryset
+    
+
+class PostDetail(generics.ListAPIView):
+    serializer_class= PostSerializer
+    
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        title = self.kwargs["title"]
+        queryset = queryset.filter(post__title=title)
+        return queryset
+ 
+ 
