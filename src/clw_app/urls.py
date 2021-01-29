a2b48b050f97b88de03f4e35bcf19614e4bfd40b
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2021-01-29 14:41:13 -0500

    added login/logout

--- src/clw_app/urls.py
+++ src/clw_app/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),
     path("blog/", include("blog.urls")),
     path("user/", include("users.urls"))
     
